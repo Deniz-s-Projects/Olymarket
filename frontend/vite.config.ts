daab083070b5ba67f6ce71c9499b893cf7a6e197
NothingSus <64636789+StateofDisarray@users.noreply.github.com>
2025-10-31 21:40:11 +0100

    fix

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+      host: '0.0.0.0', // This exposes the server to your network
+      port: 5173,      // (Optional) Explicitly set the port
+    }
+    // ⬆️ END CONFIGURATION BLOCK ⬆️
+  })
